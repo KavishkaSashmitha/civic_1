e1487175917886d13f250198e3968be12664fa41
Thisara <sasmithajayasinghe1@gmail.com>
2024-08-20 18:23:26 +0530

    incident

--- lib/screens/incidentlist_page.dart
+++ lib/screens/incidentlist_page.dart
@@ -1,4 +1,5 @@
 import 'package:civic_1/components/pulsating_live_button.dart';
+import 'package:civic_1/screens/Incident_Detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:toggle_switch/toggle_switch.dart';
@@ -74,102 +75,113 @@ class ActiveCasesScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.only(bottom: 16.0),
-            child: Card(
-              color: const Color.fromARGB(255, 61, 52, 52),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              top: 13.0, bottom: 8.0, left: 15),
-                          child: Text(
-                            'Report of Bribery Incident',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 20,
-                              height: 1.1,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15),
-                          child: Text(
-                            'Spotted Bribery Incident at of Department of Immigration...',
-                            style: TextStyle(
-                              color: Colors.white70,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0, left: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Icon(
-                                Icons.location_pin,
-                                color: Colors.red,
-                              ),
-                              SizedBox(width: 8),
-                              Text(
-                                'Department of Immigration',
-                                style: TextStyle(
-                                  color: Colors.orange,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 10, bottom: 10, right: 10),
-                    child: SizedBox(
-                      width: 60, // Set a fixed width for the image column
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => IncidentReportScreen()),
+                );
+              },
+              child: Card(
+                color: const Color.fromARGB(255, 61, 52, 52),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.network(
-                              'https://media.baamboozle.com/uploads/images/659418/1682965619_515_url.png',
-                              width: 60,
-                              height: 60,
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Center(
-                                    child: Text('Image failed to load'));
-                              },
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 13.0, bottom: 8.0, left: 15),
+                            child: Text(
+                              'Report of Bribery Incident',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 20,
+                                height: 1.1,
+                              ),
                             ),
                           ),
-                          SizedBox(
-                              height: 8), // Space between image and "Live" text
-                          Text(
-                            'Live',
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontWeight: FontWeight.bold,
+                          SizedBox(height: 4),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Text(
+                              'Spotted Bribery Incident at of Department of Immigration...',
+                              style: TextStyle(
+                                color: Colors.white70,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(bottom: 8.0, left: 15),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.location_pin,
+                                  color: Colors.red,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Department of Immigration',
+                                  style: TextStyle(
+                                    color: Colors.orange,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 10, bottom: 10, right: 10),
+                      child: SizedBox(
+                        width: 60, // Set a fixed width for the image column
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.network(
+                                'https://th.bing.com/th/id/OIP.iEiyLn0VVjVe8qknIDtwbQHaFO?w=1899&h=1340&rs=1&pid=ImgDetMain',
+                                width: 60,
+                                height: 60,
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Center(
+                                      child: Text('Image failed to load'));
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                                height:
+                                    8), // Space between image and "Live" text
+                            Text(
+                              'Live',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
